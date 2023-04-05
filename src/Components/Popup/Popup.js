f97b66d349a8af1977b0aa1c28e11fe275e1c4f8
Rafael Amaral <rafaelbmo@live.com>
2023-04-05 14:06:55 -0300

    update propaganda bruno

--- src/Components/Popup/Popup.js
+++ src/Components/Popup/Popup.js
@@ -2,6 +2,8 @@ import React, { useState, useRef } from 'react';
 import Popup from './Popup2';
 import './Popup.css';
 import DOC from '../Popup/modelo-projeto-integrado.doc'
+import Propaganda from './propaganda-bruno.jpeg'
+import Alerta from './alerta.png'
 
 
 function App() {
@@ -36,18 +38,51 @@ function App() {
         <div className="App">
             {showPopup && (
                 <Popup ref={popupRef} closePopup={handleClosePopup}>
-                    <p style={{ fontSize: '20px' }}><a href={DOC}   rel="noopener noreferrer" onClick={handleLinkClick}>DOWNLOAD MODELO PROJETO INTEGRADO!</a></p>
+                    <div
+                        className='Aviso'>
+
+                        <p>
+                            Não consegue ler? Toque na imagem para ampliar
+                        </p>
+                    </div>
+
+                    <div className="popup-image">
+                        <a href="https://i.imgur.com/IWZ5SVP.jpg" target="_blank">
+                            <img src={Propaganda} alt="Minha imagem" className="popup-image" />
+                        </a>
+
+                    </div>
+                    <div
+                        className='Aviso2'>
+
+                        <a href="https://encurtador.com.br/zAQUX" target="_blank">
+                            <p style={{ color: 'green', textDecoration: 'underline' }}>
+                                GARANTIR AGORA MEU PROJETO INTEGRADO!!!
+                            </p>
+                        </a>
+                    </div>
+
+                    <p style={{ fontSize: '20px', textAlign: 'center' }}>
+                        <a href={DOC} rel="noopener noreferrer" onClick={handleLinkClick}>
+                            DOWNLOAD MODELO PROJETO INTEGRADO!
+                        </a>
+                    </p>
 
 
-                    <p>
+                    {/* 
+                     <p>
                         <span style={{ fontSize: '18px', color: 'tomato' }}>
                             Considere ajudar o site fazendo uma doação PIX!<br />
-                            localizado abaixo do botão RESPOSTAS!!
+                            localizado abaixo do botão RESPOSTAS!!!
                         </span>
                     </p>
+                    */}
+
+
                 </Popup>
             )}
         </div>
+
     );
 }
 
